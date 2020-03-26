379776a5e564d79d3f828eab95e0b59d5ce8d0c8
Matteo Carpi <matteocarpi94@gmail.com>
2020-03-26 12:22:36 +0100

    feat: home scaffoldings

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
+import BackgroundImage from 'gatsby-background-image';
 import styles from '../styles/Home.module.scss';
 import { graphql } from 'gatsby';
 import PropTypes from 'prop-types';
@@ -10,22 +11,47 @@ import PropTypes from 'prop-types';
 const Home = ({data}) => (
   <Layout>
     <SEO title="Home" />
-    <div className={styles.content}>
-      <h1>{data.site.siteMetadata.title}</h1>
-    </div>
+    <BackgroundImage
+      fluid={data.allMarkdownRemark.edges[0].node.frontmatter.main_image.childImageSharp.fluid}
+      backgroundColor={'#000000'}
+      className={styles.introSlide}
+    >
+      <div className={styles.overlay}>
+        <h1>{data.site.siteMetadata.title}</h1>
+        <h2>{data.site.siteMetadata.description}</h2>
+      </div>
+    </BackgroundImage>
   </Layout>
 );
 
 export default Home;
 
 export const query = graphql`
-query HomeQuery {
-  site {
-    siteMetadata {
-      title
+    query HomeQuery {
+      site {
+        siteMetadata {
+          title
+          description
+        }
+      }
+      allMarkdownRemark {
+        edges {
+          node {
+            id
+            frontmatter {
+              main_image {
+                 childImageSharp {
+                  fluid {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
     }
-  }
-}`;
+  `;
 
 Home.propTypes = {
   data: PropTypes.node,
