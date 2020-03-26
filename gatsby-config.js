3f0663377d53bab2a4020b191e13ee79d64f26f8
Matteo Carpi <matteocarpi94@gmail.com>
2020-03-26 18:20:45 +0100

    fix: source markdown files separately

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,9 +53,17 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        name: 'content',
+        name: 'posts',
         // eslint-disable-next-line no-undef
-        path: `${__dirname}/content`,
+        path: `${__dirname}/content/posts`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        // eslint-disable-next-line no-undef
+        path: `${__dirname}/content/pages`,
       },
     },
     // {
