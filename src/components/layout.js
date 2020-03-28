efbe880bd32cc6fc8e570156c6d7d4cc2d8ebfbe
Matteo Carpi <matteocarpi94@gmail.com>
2020-03-28 15:32:26 +0100

    feat: styled homepage

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,13 +8,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from './header';
+import { useStaticQuery, graphql } from 'gatsby';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faFacebookF } from '@fortawesome/free-brands-svg-icons';
+
+import styles from '../styles/Layout.module.scss';
 
 const Layout = ({ children }) => {
-
+  const data = useStaticQuery(graphql`
+  query LayoutQuery {
+    homeData: allMarkdownRemark(filter: {fields: {slug: {eq: "/home/"}}}) {
+      edges {
+        node {
+          frontmatter {
+            instagram
+            facebook
+          }
+        }
+      }
+    }
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`);
   return (
     <>
       <Header />
       <main>{children}</main>
+      <footer>
+        <span>©2020 Niall Fallon</span>
+        <div className={styles.social}>
+          <a className={styles.iconWrap} href={data.homeData.edges[0].node.frontmatter.instagram}>
+            <FontAwesomeIcon className={styles.socialIcon} icon={faInstagram} />
+          </a>
+          <a className={styles.iconWrap} href={data.homeData.edges[0].node.frontmatter.instagram}>
+            <FontAwesomeIcon className={styles.socialIcon} icon={faFacebookF} />
+          </a>
+        </div>
+      </footer>
     </>
   );
 };
