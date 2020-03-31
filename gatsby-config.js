b398a3ecc33b3f405baf9ac9873da55a9d88f514
Matteo Carpi <matteocarpi94@gmail.com>
2020-03-31 19:05:12 +0200

    chore: installed gastby remark images

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape */
 // eslint-disable-next-line no-undef
 module.exports = {
   siteMetadata: {
@@ -6,6 +7,22 @@ module.exports = {
     author: '@matteocarpi',
   },
   plugins: [
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 1024,
+            },
+          },
+        ],
+      },
+    },
     {
       resolve: 'gatsby-transformer-remark',
       options: {
