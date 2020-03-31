0bb963dafde98d50d2eeb76d23b99c08fa1c5e6c
Matteo Carpi <matteocarpi94@gmail.com>
2020-03-31 19:00:20 +0200

    feat: about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,8 +3,8 @@ import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-import styles from '../styles/Home.module.scss';
-import { graphql, Link } from 'gatsby';
+import styles from '../styles/About.module.scss';
+import { graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 import Img from 'gatsby-image';
 import Markdown from 'markdown-to-jsx';
@@ -12,62 +12,17 @@ import Markdown from 'markdown-to-jsx';
 const About = ({data}) => (
   <Layout>
     <SEO title="Home" />
-    <h1>About</h1>
+    <h1 className={styles.title}>About</h1>
 
     <section id="about" className={styles.about}>
-      <Img className={styles.headshot} fluid={data.homeData.edges[0].node.frontmatter.headshot.childImageSharp.fluid} />
+      <Img className={styles.headshot} fluid={data.aboutData.edges[0].node.frontmatter.headshot.childImageSharp.fluid} />
       <div className={styles.bio}>
         <Markdown>
-          {data.homeData.edges[0].node.frontmatter.about}
+          {data.aboutData.edges[0].node.frontmatter.about}
         </Markdown>
       </div>
     </section>
 
-    <section id="projects" className={styles.projects}>
-      <h1 className="sectionTitle">Projects</h1>
-
-      <div className={styles.projectList}>
-        {data.projectsData.edges.map((project) => {
-          return (
-            <Link className={styles.projectWrap} to={project.node.fields.slug} key={project.node.frontmatter.title}>
-              <Img className={styles.project}fluid={project.node.frontmatter.gallery[0].childImageSharp.fluid}/>
-              <div className={styles.projectInfo}>
-                <h2>
-                  {project.node.frontmatter.title}
-                </h2>
-              </div>
-            </Link>
-          );
-        })}
-      </div>
-    </section>
-
-    <section id="contacts" className={styles.contacts}>
-      <h1 className="sectionTitle">Contact</h1>
-      <a 
-        href={`mailto:${data.homeData.edges[0].node.frontmatter.email}`}
-      >
-        {data.homeData.edges[0].node.frontmatter.email}
-      </a>
-    </section>
-
-    <section className={styles.gallery}>
-      {data.homeData.edges[0].node.frontmatter.gallery.map((image) => {
-        return <Img key={image.id} className={styles.galleryImage} fluid={image.childImageSharp.fluid} />;
-      })}
-    </section>
-
-    <section className={styles.upcoming}>
-      <h1 className="sectionTitle">Upcoming Performances</h1>
-      <ul>
-        {data.homeData.edges[0].node.frontmatter.upcoming_performances.map((performance) => {
-          return (
-            <li key={performance} className={styles.performance}>{performance}</li>
-          );
-        })}
-      </ul>
-    </section>
-    
   </Layout>
 );
 
@@ -81,60 +36,18 @@ export const query = graphql`
           description
         }
       }
-      projectsData: allMarkdownRemark(filter: {fields: {slug: {nin: ["/home/", "/about/"]}}} ) {
-        edges {
-          node {
-            fields {
-              slug
-            }
-            frontmatter {
-              title
-              gallery {
-                id
-                childImageSharp {
-                  fluid {
-                    ...GatsbyImageSharpFluid
-                  }
-                }
-              }
-            }
-            internal {
-              content
-            }
-          }
-        }
-      }
-      homeData: allMarkdownRemark(filter: {fields: {slug: {eq: "/home/"}}}) {
+      aboutData: allMarkdownRemark(filter: {fields: {slug: {eq: "/about/"}}}) {
         edges {
           node {
             frontmatter {
               about
               headshot {
                 childImageSharp {
-                  fluid (maxWidth: 300, maxHeight: 300, cropFocus: CENTER){
-                    ...GatsbyImageSharpFluid
-                  }
-                }
-              }
-              main_image {
-                childImageSharp {
                   fluid {
                     ...GatsbyImageSharpFluid
                   }
                 }
               }
-              gallery {
-                id
-                childImageSharp {
-                  fluid {
-                    ...GatsbyImageSharpFluid
-                  }
-                }
-              }
-              email
-              facebook
-              instagram
-              upcoming_performances
             }
           }
         }
