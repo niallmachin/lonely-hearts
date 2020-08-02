12401bdd7eb13de4c4f03efe5f8af4b90fd2a16f
Matteo Carpi <matteocarpi94@gmail.com>
2020-08-02 12:42:58 +0200

    feat: submenu for projects

--- src/components/header.js
+++ src/components/header.js
@@ -12,6 +12,18 @@ const Header = () => {
           title
         }
       }
+      allMarkdownRemark(filter: {frontmatter: {template: {eq: "project"}}}) {
+        edges {
+          node {
+            frontmatter {
+              title
+            }
+            fields {
+              slug
+            }
+          }
+        }
+      }
     }
   `);
   return (
@@ -21,7 +33,24 @@ const Header = () => {
         <ul>
           {navigation.map((navItem) => {
             return (
-              <li key={navItem.name}><Link className={styles.navLink} to={navItem.url}>{navItem.name}</Link></li>
+              <li key={navItem.name}>
+                <Link className={styles.navLink} to={navItem.url}>
+                  {navItem.name}
+                </Link>
+                {navItem.name === 'Projects' && 
+                  
+                  <ul className={styles.subMenu}>
+                    {data.allMarkdownRemark.edges.map(project => {
+                      const pj = project.node;
+                      return (
+                        <li key={pj.fields.slug}>
+                          <Link to={pj.fields.slug}>{pj.frontmatter.title}</Link>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                }
+              </li>
             );
           })}
         </ul>
