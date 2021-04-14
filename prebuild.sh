173bc2271678a2414459140c34a4e3982db1b26c
Matteo Carpi <matteocarpi94@gmail.com>
2021-04-14 10:13:55 +0200

    Improved deploy script

--- prebuild.sh
+++ prebuild.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl --location --request DELETE "https://$DRONE_SYSTEM_HOST/api/repos//builds/" --header "Authorization: Bearer $DRONE_AUTH_TOKEN"
+curl --location --request DELETE "https://$DRONE_SYSTEM_HOST/api/repos/$DRONE_REPO/builds/`expr $DRONE_BUILD_NUMBER - 1`" --header "Authorization: Bearer $DRONE_AUTH_TOKEN"
